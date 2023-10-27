36b3d9b0378623d01e93b64134bb7e72bcaa72d2
dalexbowers <davibowe@ttu.edu>
2023-10-27 10:46:42 -0500

    added elevation plots

--- scripts/age_structure.R
+++ scripts/age_structure.R
@@ -1,17 +1,52 @@
+# Alex Bowers
+
 library(geosphere)
 library(ggplot2)
+library(elevatr) 
 
 # merge field data with ring counts
-tree_ages <- dplyr::left_join(trees, ages, by = "id")
+trees <- dplyr::left_join(trees, ages, by = "id")
 
 
-# distance from transect start
-samplingpoint <- data.frame(long=tree_ages$long, lat=tree_ages$lat)
-transectpoint <- data.frame(long=tree_ages$transect_long, lat=tree_ages$transect_lat)
-tree_ages$distance <- distHaversine(samplingpoint, transectpoint)
 
-# all plots age structure
-ggplot(tree_ages, aes(distance, year, color=transect_id)) + 
+
+### distance from transect start
+samplingpoint <- data.frame(long=trees$long.x, lat=trees$lat.x)
+transectpoint <- data.frame(long=trees$transect_long, 
+                            lat=trees$transect_lat)
+trees$distance <- distHaversine(samplingpoint, transectpoint)
+
+
+
+
+### elevation
+
+# function to find elevation for every poi t
+# gets every coordinates elevation
+get_cords <- function(long, lat, id) {
+  sample_pts <- data.frame(x = long, 
+                           y = lat, 
+                           names = id)
+  # wgs 84
+  elev <- get_elev_point(locations = sample_pts, prj = 4326)
+  return(elev[[3]])
+}
+
+# finds different in elevation between trasnect start and sampling point
+trees$sample_elev <- get_cords(trees$long.x, trees$lat.x, trees$id)
+trees$transect_elev <- get_cords(trees$transect_long, trees$transect_lat, trees$id)
+
+trees$elev_dif <- trees$sample_elev - trees$transect_elev
+
+
+
+
+### plots
+
+## disance plots 
+
+# all plots age structure 
+ggplot(trees, aes(distance, year, color=transect_id)) + 
   geom_point() +
   facet_wrap(~transect_id) +
   labs(
@@ -20,18 +55,53 @@ ggplot(tree_ages, aes(distance, year, color=transect_id)) +
     color = "Transect"
   )
 
-# plot all sites together
-ggplot(tree_ages, aes(distance, year, color=property_id)) + 
+# dbh vs distance 
+ggplot(trees, aes(distance, dbh, color=transect_id)) + 
+  geom_point() +
+  facet_wrap(~transect_id) +
+  labs(
+    x = "Distance from start of transect (m)",
+    y = "DBH",
+    color = "Transect"
+  )
+
+# plot all sites together age vs distance
+ggplot(trees, aes(distance, year, color=property_id)) + 
   geom_point(size=4) + 
   labs(
     x = "Distance from start of transect (m)",
-    y = "Ring Count",
+    y = "Ring count",
+    color = "Property"
+  ) +
+  theme_bw(base_size = 18)
+ # all sites together age vs distance
+ggplot(trees, aes(distance, dbh, color=property_id)) + 
+  geom_point(size=4) + 
+  labs(
+    x = "Distance from start of transect (m)",
+    y = "Diameter at breast height (cm)",
     color = "Property"
   ) +
   theme_bw(base_size = 18)
 
-# distribution of ages
-hist(tree_ages$year, breaks = 10, xlab="Ring Count", freq = TRUE,
-     main="", xlim=c(0,250), ylim = c(0,50))
 
-summary(tree_ages)
+## elevation plots
+# all together 
+ggplot(trees, aes(elev_dif, year, color=property_id)) + 
+  geom_point(size=4) + 
+  labs(
+    x = "Elevation from start of transect (m)",
+    y = "Ring count",
+    color = "Property"
+  ) +
+  theme_bw(base_size = 18)
+
+# seperated by site
+ggplot(trees, aes(elev_dif, year, color=transect_id)) + 
+  geom_point() +
+  facet_wrap(~transect_id) +
+  labs(
+    x = "Elevation from start of transect (m)",
+    y = "Ring Count",
+    color = "Transect"
+  )
--- scripts/trees_read_clean.R
+++ scripts/trees_read_clean.R
@@ -1,5 +1,7 @@
 # Alex Bowers
 
+
+
 ## cleans trees.csv for analysis
 
 # load sampling and prperty data
@@ -17,5 +19,6 @@ trees <- merge(trees, transect_starts, by = c("property_id", "transect"))
 trees$individual <- stringr::str_pad(trees$individual, 2, pad = "0")
 
 # creates sample id and transect id
-trees$id <- paste(trees$property_code, trees$transect, trees$individual, sep="")
+trees$id <- paste(trees$property_code, trees$transect, 
+                  trees$individual, sep="")
 trees$transect_id <- paste(trees$property_code, trees$transect, sep="")
