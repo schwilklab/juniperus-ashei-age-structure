0b63c1501457133aba9222659dd29a8053e35967
dalexbowers <davibowe@ttu.edu>
2024-02-21 16:53:05 -0600

    updated figures

--- scripts/transect_map.R
+++ scripts/transect_map.R
@@ -27,12 +27,21 @@ elev <- elev[elev$y >= 30.07,]
 
 
 ggplot(trees_keb, aes(long.x, lat.x)) + 
+  geom_contour_filled(data = elev, aes(x, y, z = elev), bins = 10, 
+                      show.legend = TRUE) +
+  scale_fill_manual(values = c("#D7D7C8", "#A5AA8C", "#C8C3A0", "#E1D2A2", "#D2B496", 
+                      "#CDA087", "#AF7873", "#A05555", "#914137"),
+                    labels = c("580-586", "586-590", "592-595", "596-600", "601-605", 
+                               "606-610", "611-615", "616-620", "621-625")) +
   geom_point(size = 3) +
-  geom_contour(data = elev, aes(x, y, z = elev), bins = 10) +
   xlim(-99.402, -99.4) +
   ylim(30.07, 30.0725) + 
   theme_bw() +
+  labs(fill = "Elevation (m)") +
   theme(
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 16),
+    text = element_text(family = "Times New Roman"),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     axis.ticks.x=element_blank(), 
